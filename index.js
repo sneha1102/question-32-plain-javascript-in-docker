cd093ac1ac5cf014e60ddaa88163e737bd9e7b5d
sneha1102 <36694331+sneha1102@users.noreply.github.com>
2020-11-11 11:17:54 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 console.log(API_URL);
 
 function renderDynamicContent() {
-    document.getElementById('dynamic-content').innerHTML = "Dynamic content"
+  let el = document.createElement("input");
+  let elee = document.createElement("input");
+  let e = document.createElement("input");
+  let ele = document.createElement("button");
+  el.setAttribute("id", "text1");
+  el.setAttribute("id", "txtresult");
+
+  elee.setAttribute("id", "text2");
+
+  ele.setAttribute("id", "click");
+  ele.setAttribute("name", "btnclick");
+  document.getElementById("dynamic-content").appendChild(e);
+  document.getElementById("dynamic-content").appendChild(el);
+  document.getElementById("dynamic-content").appendChild(ele);
+  document.getElementById("dynamic-content").appendChild(elee);
 }
 
+document.getElementById("click").addEventListener("click", click);
+function click() {
+  let one = document.getElementById("text1").innerText;
+  let two = document.getElementById("text2").innerText;
+  let res = parseFloat(one) + parseFloat(two);
+  document.getElementById("txtresult").innerText = res.toString();
+}
